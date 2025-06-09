c3523bca0f7980e75aa0bdf48c68b06e7b07461a
anastasia38 <naste38@gmail.com>
2025-06-09 16:44:11 +0300

    Added HW4 tasks

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let str = "Interesting Home Work";
+const number = "28";
+const isNumber = typeof number === "29";
+const data = null;
+let b;
+
+console.log(str);
+console.log(number);
+console.log(isNumber);
+console.log(data);
+console.log(b);
---
+++ jsBasics/task4.3.js
@@ -0,0 +1,8 @@
+const radius = 5;
+const height = 10;
+const volume = Math.PI * (radius ** 2) * height;
+const roundedVolume = volume.toFixed(2);
+
+console.log("Radius:", radius);
+console.log("Height:", height);
+console.log("Volume of cylinder:", roundedVolume);
