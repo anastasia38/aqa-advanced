8d85a9782845622adc04a902cca4d8657a568cd8
anastasia38 <naste38@gmail.com>
2025-06-14 21:20:57 +0300

    Added task1, task2, task3 for HW5

---
+++ Homework5/task1.js
@@ -0,0 +1,17 @@
+const averageGrade = 71;
+
+
+if (averageGrade < 60) {
+    console.log("незадовільно");
+} else if (averageGrade <= 70) {
+    console.log("Задовільно");
+} else if (averageGrade <= 80) {
+    console.log("Добре");
+} else if (averageGrade <= 90) {
+    console.log("Дуже добре");
+} else if (averageGrade <= 100) {
+    console.log("Відмінно");
+} else {
+    console.log("Некоректне значення оцінки");
+}
+
